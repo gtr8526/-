f5dc5fabda9ad67c4532a836d6598f63746c0153
Adrian <drffgrv@gmail.com>
2023-11-04 17:40:19 +0200

    first commit2

--- js/sript.js
+++ js/sript.js
@@ -35,3 +35,73 @@ function setScrollToOptions() {
         behavior:"smooth"
     });
 }
+function setScrollTo(){
+  window.scrollTo(0, 50);
+  const windowScrollTop = window.pageYOffset;
+  console.log(windowScrollTop);  
+}  
+function setScrollToOptions(){
+    window.scrollTo({
+        top: 500,
+        left:0,
+        behavior:"smooth"
+    })
+}
+function setScrollIntoView (top) {
+    const lessonSelected = document.querySelector('.lesson');
+    lessonSelected.scrollIntoView(top);
+}
+function setScrollIntoViewOptions (top) {
+    const lessonSelected = document.getElementById
+    lessonSelected.scrollIntoView({
+    block:"center",
+    inline:"nearest",
+    behavior:"smooth"
+    });
+}
+const block = document.querySelector('.lesson_item_sub-list1');
+const elementOffsetParent = block.OffsetParent;
+console.log(elementOffsetParent);
+const elementOffsetLeft = block.OffsetLeft;
+const elementOffsetTop  = block.offsetTop;
+console.log(elementOffsetLeft);
+console.log(elementOffsetTop);
+const elementOffsetWidh = block.offsetWidth;
+const elementOffsetHeight = block.offsetHeight;
+console.log(elementOffsetWidh);
+console.log(elementOffsetHeight);
+const elementClientTop = block.clientTop;
+const elementClientLeft = block.clientLeft;
+console.log(elementClientTop);
+console.log(elementClientLeft);
+const elementClientWidh = block.clientWidth;
+const elementClientHeight = block.clientHeight;
+console.log(elementClientWidh);
+console.log(elementClientHeight);
+const elementScrollWidth = block.scrollWidth;
+const elementScrollHeight = block.scrollHeight;
+console.log(elementScrollWidth);
+console.log(elementScrollHeight);
+block.scrollTop = 150;
+const elementScrollLeft = block.scrollLeft;
+const elementScrollTop = block.scrollTop;
+console.log(elementScrollLeft);
+console.log(elementScrollHeight);
+function setElementScrollBy() {
+    block.ScrollBy({
+        top:20,
+        left:0,
+        behavior:"smooth"
+    })
+}
+const item = document.querySelector('.lesson_item_sub-list2');
+const itemCoords = item.getBoundingClientRect();
+console.log(itemCoords);
+const getItemCoords = item.getBoundingClientRect().left;
+console.log(getItemCoords);
+const getItemTopCoords = item.getBoundingClientRect().top;
+const getItemTopDocumentCoords = getItemTopCoords + window.pageYOffset;
+console.log(getItemTopCoords);
+console.log(getItemTopDocumentCoords);
+const elem = document.elementFromPoint(100,100);
+console.log(elem);
